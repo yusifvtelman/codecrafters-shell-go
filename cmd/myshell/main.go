52dfdab4765e31bf304fe07d2bdbcb829a3c59d5
yusifvtelman <yusifvtelman@gmail.com>
2025-02-07 21:12:42 +0400

    test

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 var _ = fmt.Fprint
@@ -11,10 +13,24 @@ var _ = fmt.Fprint
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error reading input: ", err)
 		os.Exit(1)
 	}
-	fmt.Fprintln(os.Stdout, "Command entered: ", command)
+
+	command := strings.TrimSpace(input)
+	parts := strings.Fields(command)
+	if len(parts) == 0 {
+		os.Exit(0)
+	}
+
+	cmdName := parts[0]
+	_, err = exec.LookPath(cmdName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: command not found\n", cmdName)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
 }
