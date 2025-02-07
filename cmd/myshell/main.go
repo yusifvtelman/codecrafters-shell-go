85fc855f2ece154baaf51f15e7a08aff8af016ee
yusifvtelman <yusifvtelman@gmail.com>
2025-02-07 21:03:38 +0400

    test

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,5 +10,11 @@ var _ = fmt.Fprint
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
-	bufio.NewReader(os.Stdin).ReadString('\n')
+
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input: ", err)
+		os.Exit(1)
+	}
+	fmt.Fprintln(os.Stdout, "Command entered: ", command)
 }
